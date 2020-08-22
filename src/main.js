d4fa4dc72bbad1a8e52f958076f85550eccb86f1
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2020-08-22 22:19:47 +0800

    [ 全站 ] init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
+import $ from 'jquery';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import ValidationProvider from 'vee-validate';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+// 將 $ 註冊為全域符號
+window.$ = $;
+Vue.use(VueAxios, axios);
+Vue.component('Loading', Loading);
+Vue.component('ValidationProvider', ValidationProvider);
+
+// Vue.filter('thousands', thousandsFilter);
 
 new Vue({
   router,
