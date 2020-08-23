1453401274bae19b9497cf28366d9ebdb7729d8b
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2020-08-23 13:19:10 +0800

    [ prod ] 單一產品頁 CSS

--- src/views/layout/Layout_prod.vue
+++ src/views/layout/Layout_prod.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <loading :active.sync="isLoading" color="#2D7487" background-color="#000"></loading>
-    <section class="banner banner--prods">
+    <section class="banner banner-sm banner--prod">
       <h2 class="fz-m color-tertiary font-primary">SHOP NOW</h2>
     </section>
     <section class="pt-lg pb-lg">
       <div class="container">
         <div class="row">
-          <div class="col-5">
-            <img :src="product.imageUrl" />
+          <div class="col-5 prod-gallery">
+            <img :src="product.imageUrl">
           </div>
           <div class="col-7">
             <h3 class="mb-1 fz-m font-primary ta-left color-primary">{{ product.title }}</h3>
             <div class="d-flex mb-1">
-              <del class="mr-2 fz-sm color-secondary">{{ Number(product.origin_price) | thousands }}</del>
+              <del class="mr-xs fz-xs color-secondary">{{ Number(product.origin_price) | thousands }}</del>
               <p class="fz-sm fw-bold">{{ Number(product.price) | thousands }}</p>
             </div>
-            <p class="mb-1 ta-left">{{ product.description }}</p>
+            <p class="mb-m ta-left">{{ product.description }}</p>
             <div class="d-flex mb-1">
-              <input class="input input-line" type="number" v-model="prodNum" />
+              <input class="input input-line" type="number" v-model="prodNum">
               <button class="btn btn-dark ml-1" @click="addCart(product, prodNum)">Add Cart</button>
             </div>
           </div>
@@ -47,7 +47,7 @@ export default {
       this.temporaryCart = prod;
       this.temporaryCart.product = prod.id;
       this.temporaryCart.quantity = prodNum;
-      this.$http.post(`https://course-ec-api.hexschool.io/api/${process.env.VUE_APP_UUID}/ec/shopping`, this.temporaryCart)
+      this.$http.post(`${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/ec/shopping`, this.temporaryCart)
         .then(() => {
           // eslint-disable-next-line no-undef
           this.$router.push({ path: '/palipali/cart' });
@@ -58,7 +58,7 @@ export default {
     this.isLoading = true;
     // 取得產品 id
     const { id } = this.$route.params;
-    this.$http.get(`https://course-ec-api.hexschool.io/api/${process.env.VUE_APP_UUID}/ec/product/${id}`)
+    this.$http.get(`${process.env.VUE_APP_APIPATH}/${process.env.VUE_APP_UUID}/ec/product/${id}`)
       .then((res) => {
         this.product = res.data.data;
         this.isLoading = false;
