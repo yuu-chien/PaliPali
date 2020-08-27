97727db7014ea52eeb70cb367c0f25c17bc4ea62
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2020-08-28 00:56:19 +0800

    [ products ] 刪空格

--- src/views/layout/Layout_products.vue
+++ src/views/layout/Layout_products.vue
@@ -10,57 +10,27 @@
           <menu class="col-3">
             <ul class="mt-1 font-primary fz-sm">
               <li class="cate-item mb-1">
-                <a href="#" class="links is-active" data-cate="new" @click.prevent="cateChoose($event)">New Arrive</a>
+                <a href="" class="links is-active" data-cate="new" @click.prevent="cateChoose($event)">New Arrive</a>
               </li>
               <li class="cate-item mb-1">
-                <a
-                  href="#"
-                  class="links"
-                  data-cate="Swimwear"
-                  @click.prevent="cateChoose($event)"
-                >Swimwear</a>
+                <a href="" class="links" data-cate="Swimwear" @click.prevent="cateChoose($event)">Swimwear</a>
               </li>
               <li class="cate-item mb-1">
-                <a
-                  href="#"
-                  class="links"
-                  data-cate="T-shirt"
-                  @click.prevent="cateChoose($event)"
-                >T-shirt</a>
+                <a href="" class="links" data-cate="T-shirt" @click.prevent="cateChoose($event)">T-shirt</a>
               </li>
               <li class="cate-item mb-1">
-                <a
-                  href="#"
-                  class="links"
-                  data-cate="Shirt"
-                  @click.prevent="cateChoose($event)"
-                >Shirt</a>
+                <a href="" class="links" data-cate="Shirt" @click.prevent="cateChoose($event)">Shirt</a>
               </li>
               <li class="cate-item mb-1">
-                <a
-                  href="#"
-                  class="links"
-                  data-cate="Pants"
-                  @click.prevent="cateChoose($event)"
-                >Pants</a>
+                <a href="" class="links" data-cate="Pants" @click.prevent="cateChoose($event)">Pants</a>
               </li>
               <li class="cate-item mb-1">
-                <a
-                  href="#"
-                  class="links"
-                  data-cate="Socks"
-                  @click.prevent="cateChoose($event)"
-                >Socks</a>
+                <a href="" class="links" data-cate="Socks" @click.prevent="cateChoose($event)" >Socks</a>
               </li>
             </ul>
           </menu>
           <div class="col-9 d-flex flex-wrap-w p-0">
-            <div
-              class="col-4 card pointer mb-1"
-              v-for="(item, id) in productsData"
-              :key="id"
-              @click.prevent="getAProducts(item)"
-            >
+            <div class="col-4 card pointer mb-1" v-for="(item, id) in productsData" :key="id" @click.prevent="getAProducts(item)">
               <div class="card-head prod-pic">
                 <img :src="item.imageUrl" />
               </div>
